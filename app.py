dfaedf70b2faa418be8e9b871e1eb6a91dc7bbcf
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-02-25 20:12:34 +0530

    connected mysql

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import time
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 app = Flask(__name__)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/samay'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/samay'
 db = SQLAlchemy(app)
 
 class Contact(db.Model):
     
-    email = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120),  primary_key=True)
     phone = db.Column(db.String(12), nullable=False)
     message = db.Column(db.String(120), nullable=False)
 
@@ -38,8 +41,8 @@ def about():
 def features():
     return render_template('features.html')
 
-@app.route('/contact')
-def contact():
+@app.route('/contact_page')
+def contact_page():
     return render_template('contact.html')
 
 @app.route('/explore')
